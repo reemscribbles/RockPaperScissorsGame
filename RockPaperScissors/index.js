b371a51634a13f7384069dfa19631cdf0c011cfc
reemscribbles <88132690+reemscribbles@users.noreply.github.com>
2024-04-19 15:00:47 +0300

    Add files via upload

---
+++ RockPaperScissors/index.js
@@ -0,0 +1,44 @@
+// ROCK PAPER SCISSORS
+
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
+let playerImage = "&#x270A";
+let computerImage = "&#x270A";
+
+
+function startGame(playerChoice){
+    console.log("Hleoeooeoeoeo");
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if(playerChoice === computerChoice){
+        result = "IT'S A TIE!";
+    }
+    else{
+        switch(playerChoice){
+            case "rock":
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+        }
+    }
+
+    playerDisplay.textContent = `PLAYER: ${playerImage}`;
+    computerDisplay.textContent = `Computer: ${computerImage}`;
+    resultDisplay.textContent = result;
+
+
+
+
+}
